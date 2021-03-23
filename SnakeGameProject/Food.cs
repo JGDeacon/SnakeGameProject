a68b224207567dc4bafb1373f87d697f2286b099
Rochelle Deulley <rochelle.deulley@gmail.com>
2021-03-23 16:33:14 -0500

    Set properties for game options class. Should be done. Started on food class.

---
+++ SnakeGameProject/Food.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGameProject
+{
+    class Food
+    {
+        public void Bacon { get; set; } // ~
+        public void Banana { get; set; } // ( 
+        public void CottonCandy { get; set; } // *
+        public void Person { get; set; } //165
+        public void Apple {get; set; } //decimal code 1148
+        public void Pea { get; set; } // .
+        public void Shrimp { get; set; } //decimal code 1351
+        public void Peppermint { get; set; } //decimal code 1422
+        public void Eel { get; set; } //1705
+        public void Sunshine { get; set; } //1758
+        public void Flower { get; set; } //1780
+}
---
+++ SnakeGameProject/GameOptions.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGameProject
+{
+    public enum GameBoardSize { small, medium, large }
+    public enum GameDifficulty { easy, medium, hard }
+    public class GameOptions
+    {
+        //Properties
+        public GameBoardSize BoardSize { get; set; }
+
+        public GameDifficulty Difficulty { get; set; }
+        public int BoardWidth 
+        { 
+            get
+            { 
+                return GetBoardWidth(BoardSize); 
+            } 
+        }      
+        
+        public int BoardHeight 
+        {
+            get
+            {
+                return GetBoardHeight(BoardSize);
+            } 
+        }
+        //Methods
+        private int GetBoardHeight(GameBoardSize gameBoardSize)
+        {
+            int value = 0;
+            switch (gameBoardSize)
+            {
+                case GameBoardSize.small:
+                    value = 20;
+                    return value;
+                case GameBoardSize.medium:
+                    value = 30;
+                    return value;
+                case GameBoardSize.large:
+                    value = 40;
+                    return value;
+                default:
+                    value = 20;
+                    return value;
+            }
+        }
+
+        
+
+        public int GetBoardWidth(GameBoardSize gameBoardSize) 
+        {
+            int value = 0;
+            switch (gameBoardSize)
+            {                
+                case GameBoardSize.small:
+                    value = 30;
+                    return value;
+                case GameBoardSize.medium:
+                    value = 40;
+                    return value;
+                case GameBoardSize.large:
+                    value = 50;
+                    return value;
+                default:
+                    value = 30;
+                    return value;
+            }
+
+
+        }
+        public GameOptions()
+        {
+
+        }
+        public GameOptions(GameBoardSize boardSize, GameDifficulty difficulty)
+        {
+            BoardSize = boardSize;
+            Difficulty = difficulty;
+            
+        }
+
+    }   
+}
